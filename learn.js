c4bfe653eb0462d138f7b8bad1e32800524d0ce2
Lokesh vishwakarma <lokeshvishwakarma5755@gmail.com>
2026-03-17 23:10:13 +0700

    Portfolio

---
+++ learn.js
@@ -0,0 +1,13 @@
+    // (function() {
+    //   emailjs.init("5v6eLeHQm_wHqs5uz"); // <-- यहां अपना Public Key डालें
+    // })();
+
+    // document.getElementById("contact-form").addEventListener("submit", function(event) {
+    //   event.preventDefault();
+    //   emailjs.sendForm("service_ga3vwqo", "YOUR_TEMPLATE_ID", this)
+    //     .then(function() {
+    //       alert("✅ Message Sent Successfully!");
+    //     },  function(error) {
+    //       alert("❌ Failed to Send: " + JSON.stringify(error));
+    //     });
+    // });
